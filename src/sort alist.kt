018880ff8df404785133501f734f8010a1c33f27
fatma hassan <nihankamal2020@gmail.com>
2020-08-29 22:11:15 +0300

    code example to sort a bellow list in ascending and descending

---
+++ src/sort alist.kt
@@ -0,0 +1,15 @@
+fun main() {
+// Ascending
+    val lists = listOf(8, 4, 7, 1, 2, 3, 0, 5, 6)
+    var sortedList1 = lists.sorted()
+    print("Ascending    ")
+    println(sortedList1)
+
+
+//Descending
+    print("Descending   ")
+    var sortedList = lists.sortedDescending()
+   println(sortedList);
+
+
+}
